a0a0f553bbd9fbfe7d0c91dbaa1a8988cd1eb507
nwachter <malaikaninaw@yahoo.fr>
2026-02-13 12:03:37 +0100

    motly finished

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 require('dotenv').config({ path: '.env' });
 
 const app = express();
-const port = 3000;
+const port = 4000;
 
 const URI = process.env.MONGODB_URI;
 const PORT = Number(process.env.PORT || 4000);
@@ -10,9 +10,12 @@ const NODE_ENV = process.env.NODE_ENV || 'production';
 
 const hostname = NODE_ENV === 'development' ? 'localhost' : '0.0.0.0';
 
+//A enlever en prod
+const cors = require('cors');
+app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/api/hello', (req, res) => {
+  res.json({ message: "Hello World!" });
 });
 
 app.listen(port, () => {
